747b34eaec48822572b46cc0b96a73fb4e141c3a
windows <veeranki2014@gmail.com>
2026-01-25 00:49:33 -0600

    frontend

---
+++ cart.sh
@@ -0,0 +1,17 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+#put it in starting
+cp cart.repo /etc/systemd/system/cart.service
+
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+cd /app
+unzip /tmp/cart.zip
+cd /app
+npm install
+
+systemctl daemon-reload
+systemctl enable cart
+systemctl start cart
---
+++ catalogue.sh
@@ -0,0 +1,23 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+#starting because changing the location.
+cp catalogue /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+
+
+dnf install mongodb-mongosh -y
+mongosh --host mongodb-dev.veerankitek.in </app/db/master-data.js
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
