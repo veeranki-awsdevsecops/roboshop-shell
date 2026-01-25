9f809aa4a820a8965d097ddbf3e2bf3c3c91c023
windows <veeranki2014@gmail.com>
2026-01-24 23:23:24 -0600

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,13 @@
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
+#Create nginx reverse proxy configuration.
+cp nginx.conf /etc/nginx/nginx.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-#Create nginx reverse proxy configuration.
-cp nginx.conf /etc/nginx/nginx.conf
+
 systemctl enable nginx
 systemctl start nginx
-systemctl restart nginx
+
