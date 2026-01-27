39fd3b72ad1088c19550a401c225a21479edc493
windows <veeranki2014@gmail.com>
2026-01-26 23:07:54 -0600

    roboshop code dry

--- shipping.sh
+++ shipping.sh
@@ -3,6 +3,7 @@ component=shipping
 MAVEN
 
 dnf install mysql -y
-mysql -h mysql-dev.veerankitek.in -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.veerankitek.in -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.veerankitek.in -uroot -pRoboShop@1 < /app/db/master-data.sql
+#RoboShop@1
+mysql -h mysql-dev.veerankitek.in -uroot -p$1 < /app/db/schema.sql
+mysql -h mysql-dev.veerankitek.in -uroot -p$1 < /app/db/app-user.sql
+mysql -h mysql-dev.veerankitek.in -uroot -p$1 < /app/db/master-data.sql
