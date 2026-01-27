361c065243bfca17c06ec88f718d229d15731384
windows <veeranki2014@gmail.com>
2026-01-26 23:38:55 -0600

    roboshop code dry

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,10 @@
+if [ -z "$1" ]; then
+    echo Input password is missing.
+    exit
+fi
+
+
 dnf install mysql-server -y
-
 mysql_secure_installation --set-root-pass RoboShop@1
-
 systemctl enable mysqld
 systemctl start mysqld
